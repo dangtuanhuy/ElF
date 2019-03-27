745395f578bd4694e4fa6fd37aa4f69c15731b68
Unknown <huyb1505883@student.ctu.edu.vn>
2019-03-27 08:25:50 +0700

    Location

---
+++ WorkAptech/WorkAptech/Areas/Admin/Controllers/LocationController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WorkAptech.Data;
+
+namespace WorkAptech.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class LocationController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public LocationController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        // GET: Location
+        public async Task<ActionResult> Index()
+        {
+            return View(await _db.Location.ToListAsync());
+        }
+
+        // GET: Location/Details/5
+        public async  Task<ActionResult> Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Location/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Location/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Location/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Location/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Location/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Location/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}

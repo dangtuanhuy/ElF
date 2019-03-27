a73a9abe968d1f6379397629222f2d4f674fb41b
Unknown <huyb1505883@student.ctu.edu.vn>
2019-03-27 11:48:55 +0700

    IMG

---
+++ WorkAptech/WorkAptech/Utility/SD.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorkAptech.Utility
+{
+    public class SD
+    {
+        public const string DefaultJobImage = "defaut_job.png";
+        public const string DefaultTranImage = "defaut_job.png";
+        public const string DefaultEventImage = "defaut_job.png";
+        public const string DefaultCategoryImage = "defaut_job.png";
+    }
+}

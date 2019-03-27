2dd1e2c6b75eb9f2c21bbbcd0fdaea677bc3fc14
Unknown <pnhson16074@cusc.ctu.edu.vn>
2019-03-27 09:01:12 +0700

    Add Category

--- WorkAptech/WorkAptech/Areas/Admin/Controllers/CategoryController.cs
+++ WorkAptech/WorkAptech/Areas/Admin/Controllers/CategoryController.cs
@@ -4,92 +4,124 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WorkAptech.Data;
 
 namespace WorkAptech.Areas.Admin.Controllers
 {
     [Area("admin")]
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
 
+        public CategoryController(ApplicationDbContext db) 
+        {
+            _db = db;
+        }
         // GET: Category
-        public ActionResult Index()
+        public async Task<IActionResult> Index()
+        {
+            return View(await _db.Category.ToListAsync());
+        }
+
+        //GET - CREATE
+        public IActionResult Create()
         {
             return View();
         }
-
-        // GET: Category/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        // GET: Category/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Category/Create
+        //POST - CREATE
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public async Task<IActionResult> Create(Category category)
         {
-            try
+            if (ModelState.IsValid)
             {
-                // TODO: Add insert logic here
+                //if valid
+                _db.Category.Add(category);
+                await _db.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+
+            }
+            return View(category);
+        }
+
+        //GET - EDIT
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var category = await _db.Category.FindAsync(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Update(category);
+                await _db.SaveChangesAsync();
 
                 return RedirectToAction(nameof(Index));
             }
-            catch
-            {
-                return View();
-            }
+            return View(category);
         }
 
-        // GET: Category/Edit/5
-        public ActionResult Edit(int id)
+        //GET - DELETE
+        public async Task<IActionResult> Delete(int? id)
         {
-            return View();
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var category = await _db.Category.FindAsync(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
         }
 
-        // POST: Category/Edit/5
-        [HttpPost]
+        [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public async Task<IActionResult> DeleteConfirmed(int? id)
         {
-            try
-            {
-                // TODO: Add update logic here
+            var category = await _db.Category.FindAsync(id);
 
-                return RedirectToAction(nameof(Index));
-            }
-            catch
+            if (category == null)
             {
                 return View();
             }
+            _db.Category.Remove(category);
+            await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
         }
 
-        // GET: Category/Delete/5
-        public ActionResult Delete(int id)
+        //GET - DETAILS
+        public async Task<IActionResult> Details(int? id)
         {
-            return View();
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var category = await _db.Category.FindAsync(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
         }
 
-        // POST: Category/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
     }
 }
